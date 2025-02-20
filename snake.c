3ec8c74c9f29e9a406e94fae9caa040701cb19cc
andreacaldato-hub <andreacaldato6@gmail.com>
2025-02-20 16:36:56 +0100

    ciao

--- snake.c
+++ snake.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 
 int main() {
-  // Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     printf("SDL_Init Error: %s\n", SDL_GetError());
     return 1;
   }
 
-  // Create SDL Window
   SDL_Window *window =
       SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       800, 600, SDL_WINDOW_SHOWN);
+                       1200, 1080, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP);
   if (!window) {
     printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
     SDL_Quit();
     return 1;
   }
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
 
-  // Keep window open for 5 seconds
   SDL_Delay(5000);
 
-  // Cleanup
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
