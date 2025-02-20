cd3a940c3a8ff172dc95fdf404ea3edb124b85d2
andreacaldato-hub <andreacaldato6@gmail.com>
2025-02-20 16:15:16 +0100

    ciao

--- snake.c
+++ snake.c
@@ -1,4 +1,16 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 
-int main() { printf("Ciao come va"); }
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window =
+      SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       800, 600, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    printf("Failed to open window\n");
+    return 1;
+  }
+  SDL_Delay(5000);
+}
