09a82e503fa836c9612fbd9d0489852ffdfbda9b
andreacaldato-hub <andreacaldato6@gmail.com>
2025-02-20 16:47:19 +0100

    ciao

--- snake.c
+++ snake.c
@@ -1,4 +1,6 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_stdinc.h>
 #include <stdio.h>
 
 int main() {
@@ -9,20 +11,16 @@ int main() {
 
   SDL_Window *window =
       SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       1200, 1080, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP);
+                       1200, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP);
   if (!window) {
     printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
     SDL_Quit();
     return 1;
   }
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
   SDL_RenderClear(renderer);
   SDL_RenderPresent(renderer);
 
   SDL_Delay(5000);
-
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return 0;
 }
