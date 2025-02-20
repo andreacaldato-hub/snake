6bf9231fbca2e9e01e3cd7d8f74afc76c597bc63
andreacaldato-hub <andreacaldato6@gmail.com>
2025-02-20 17:04:30 +0100

    ciao

--- snake.c
+++ snake.c
@@ -1,30 +1,20 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_pixels.h>
-#include <SDL2/SDL_rect.h>
-#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 
 int main() {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    printf("SDL_Init Error: %s\n", SDL_GetError());
-    return 1;
-  }
-
+  SDL_Init(SDL_INIT_VIDEO);
   SDL_Window *window =
-      SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       1200, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP);
-  if (!window) {
-    printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-    SDL_Quit();
+      SDL_CreateWindow("SNAKE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       1200, 900, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    printf("Errore creazione window\n");
     return 1;
   }
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-  SDL_Rect rect = {300, 200, 200, 150};
-  SDL_RenderFillRect(renderer, &rect);
   SDL_RenderPresent(renderer);
-
   SDL_Delay(5000);
 }
