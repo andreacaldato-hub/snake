de3b7a1a911f9918d0432846f851f1e9abfadeaa
andreacaldato-hub <andreacaldato6@gmail.com>
2025-02-24 21:28:05 +0100

    start

--- dimension.h
+++
@@ -1,6 +0,0 @@
-
-#define WIDTH 900
-#define HEIGHT 600
-#define CELL 30
-#define ROWS (HEIGHT / CELL)
-#define COLUMNS (WIDTH / CELL)
--- snake.h
+++
@@ -1,9 +0,0 @@
-#include <SDL2/SDL.h>
-#define MAX_SNAKE_LENGTH 100
-
-typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
-typedef struct {
-  int length;
-  SDL_Rect body[MAX_SNAKE_LENGTH];
-  Direction dir;
-} Snake;
